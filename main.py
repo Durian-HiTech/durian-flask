d9cf129ca4d84f16cbee2ca5c4de1371c15a5d49
Yinghao Zhu <zhuyinghao1999@msn.cn>
2021-07-11 00:10:14 +0800

    test: post method request demo

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ app=Flask(__name__)
 def index():
     return jsonify('Durian Here')
 
+@app.route('/test_question',methods=['POST'])
+def receive_question():
+    question_content=request.form.get('question_content')
+    res={
+        'question_content': question_content
+    }
+    return jsonify(res)
+    
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=8080, debug=True)
