138e5ee64d6bc40dc95cd00ec08eb4df43b77f8e
Yinghao Zhu <zhuyinghao1999@msn.cn>
2021-07-10 15:07:40 +0800

    ci: test gcp trigger

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def index():
-    return jsonify('Durian Flask')
+    return jsonify('Durian HiTech')
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=8080, debug=True)
